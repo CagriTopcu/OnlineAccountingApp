07c2699f729b0b6ae0a15b571dce8f5914a325ea
Çağrı Topçu <cgrtpc0@gmail.com>
2023-02-23 01:16:39 +0300

    Add project files.

---
+++ src/External/OnlineAccountingAppServer.Presentation/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineAccountingAppServer.Presentation.Abstraction;
+
+namespace OnlineAccountingAppServer.Presentation.Controllers
+{
+    public sealed class TestController : ApiController
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("İşlem başarılı");
+        }
+    }
+}
